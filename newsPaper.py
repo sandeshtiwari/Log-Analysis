1894057ac22170a1641a8501e3efeb2cda4e9dd4
Sandesh Tiwari <sandeshtiwari@live.com>
2017-09-11 10:07:39 -0500

    Created view as shortLog and column shortPath as the slug from articles tabel

--- newsPaper.py
+++ newsPaper.py
@@ -3,5 +3,5 @@ import psycopg2
 DBNAME = "news"
 db = psycopg2.connect(database=DBNAME)
 c = db.cursor()
-
+c.execute("create view shortLog as select substring(path,10) as slugPath, count(path) as num from log where path like '/article/%' group by path order by num desc")
 db.close()
