5e06728e24453bd527d4a2c726e71f3c63a37c1f
Sandesh Tiwari <sandeshtiwari@live.com>
2017-09-11 10:25:42 -0500

    Added code to display 3 most famous articles

--- newsPaper.py
+++ newsPaper.py
@@ -4,4 +4,10 @@ DBNAME = "news"
 db = psycopg2.connect(database=DBNAME)
 c = db.cursor()
 c.execute("create view shortLog as select substring(path,10) as slugPath, count(path) as num from log where path like '/article/%' group by path order by num desc")
+c.execute("select articles.title, shortLog.num from articles join shortLog on articles.slug = shortLog.slugPath limit 3")
+rows = c.fetchall()
+print("\nMost famous articles are:-\n")
+for row in rows:
+    print(row[0]+" -- "+str(row[1])+" views")
+print("\n")
 db.close()
