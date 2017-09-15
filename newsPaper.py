062af44b9ad639e4ef8a0b0d6c8d4189533a63c4
Sandesh Tiwari <sandeshtiwari@live.com>
2017-09-15 10:23:25 -0500

    Authors with most views solved

--- newsPaper.py
+++ newsPaper.py
@@ -3,10 +3,17 @@ import psycopg2
 DBNAME = "news"
 db = psycopg2.connect(database=DBNAME)
 c = db.cursor()
-c.execute("create view shortLog as select substring(path,10) as slugPath, count(path) as num from log where path like '/article/%' group by path order by num desc")
-c.execute("select articles.title, shortLog.num from articles join shortLog on articles.slug = shortLog.slugPath limit 3")
+c.execute("create view shortLogs as select substring(path,10) as slugPath, count(path) as num from log where path like '/article/%' group by path order by num desc")
+c.execute("select articles.title, shortLogs.num from articles join shortLogs on articles.slug = shortLogs.slugPath limit 3")
 rows = c.fetchall()
 print("\nMost famous articles are:-\n")
+for row in rows:
+    print(row[0]+" -- "+str(row[1])+" views")
+print("\n")
+print("Most famous authors\n")
+c.execute("create view authorArticles as select articles.author, shortLog.slugPath, shortLog.num from articles join shortLog on articles.slug = shortLog.slugPath")
+c.execute("select authors.name, sum(authorArticles.num) as n from authors join authorArticles on authors.id = authorArticles.author group by authors.name order by n desc")
+rows = c.fetchall()
 for row in rows:
     print(row[0]+" -- "+str(row[1])+" views")
 print("\n")
